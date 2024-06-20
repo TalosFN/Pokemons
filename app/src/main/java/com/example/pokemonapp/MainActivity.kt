bc372370714e4119fde87dce8676e497ff7b3502
Kirill Startsev <kirill.star.1408@gmail.com>
2024-06-20 13:11:14 +0300

    changed layout to Constraint layout and added correct null checking

--- app/src/main/java/com/example/pokemonapp/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapp/MainActivity.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokemonapp.databinding.ActivityMainBinding
+import com.example.pokemonapp.repository.PokemonRepository
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val pokemons = PokemonRepository.getPokemons()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         binding.tvNext.setOnClickListener {
             val intent = Intent(this, PokemonActivity2::class.java)
