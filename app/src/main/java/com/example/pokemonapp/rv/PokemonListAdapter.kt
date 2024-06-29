ecd6b834942705d2fd69265624e06a6fabcf3e37
Kirill Startsev <kirill.star.1408@gmail.com>
2024-06-29 17:30:07 +0300

    deleted unnecessary linear layout in margin, fixed get item count method in adapter

--- app/src/main/java/com/example/pokemonapp/rv/PokemonListAdapter.kt
+++ app/src/main/java/com/example/pokemonapp/rv/PokemonListAdapter.kt
@@ -10,22 +10,24 @@ import com.example.pokemonapp.model.Pokemon
 class PokemonListAdapter : RecyclerView.Adapter<PokemonViewHolder>() {
 
 
-       private var items = emptyList<Pokemon>()
-       private val differ = AsyncListDiffer(this, DiffCallback)
-       var onClick: (Pokemon) -> Unit = { }
+    private var items = emptyList<Pokemon>()
+    private val differ = AsyncListDiffer(this, DiffCallback)
+    var onClick: (Pokemon) -> Unit = { }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
         val view = ItemPokemonBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return PokemonViewHolder(view)
     }
-    override fun getItemCount(): Int = differ.currentList.size
+
+    override fun getItemCount(): Int = items.size
 
 
     override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
         holder.bind(items[position], onClick)
     }
-    fun submit(newItems: List<Pokemon>){
-       items = newItems
+
+    fun submit(newItems: List<Pokemon>) {
+        items = newItems
         differ.submitList(newItems)
     }
 
